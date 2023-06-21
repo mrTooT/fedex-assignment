1381d9a1dc44ecca3d1c011645084e15ecf9660c
Toetenel, Rik (ITDV UD) - KLM <Rik.Toetenel@klm.com>
2023-06-21 23:11:11 +0200

    feat(sign-up-component): Add email validation

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PasswordError, SignUpDetails } from './sign-up.model';
 import { SignUpService } from '../services/sign-up.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Component({
@@ -19,18 +19,17 @@ export class SignUpComponent {
   public invalidBookingNumber = false;
   public firstName = signal<string>('');
   public lastName = signal<string>('');
+  public email = '';
   public password = '';
-  public email = signal<string>('');
   public passwordIsStrong = true;
   public passwordError: PasswordError | null = null;
 
   constructor(
     private signUpService: SignUpService, 
     private router: Router) {
+    
+    // Angular Effect magic. When either signal updates this Effect will be called  
     effect(() => {
-      console.log(`Firstname: ${this.firstName()}`);
-      console.log(`Lastname: ${this.lastName()}`);
-      console.log(`password: ${this.password}`);
       if (this.firstName() !== '' || this.lastName() !== '' ) {
         this.passwordIsStrong = this.checkpasswordStrengh(this.password);
       }
@@ -52,29 +51,37 @@ export class SignUpComponent {
     const password = target?.value;
     this.passwordIsStrong = this.checkpasswordStrengh(password);
     this.password = password;
-    console.log('passwordIsStrong: ', this.passwordIsStrong);
   }
 
   onEmailChange(event: Event){
     const target = event?.target as HTMLInputElement;
-    this.email.set(target?.value)
+    this.email = target?.value;
   }
 
-  checkpasswordStrengh(password: string) {
+  onSubmit(): void {
+    this.isLoading = true;
+    const signUpDetails: SignUpDetails = {
+      firstName: this.firstName(),
+      lastName: this.lastName(),
+      email: this.email
+    }
+
+    this.signUpService.postSignUpDetails(signUpDetails).subscribe({
+      next: this.handleSignUpResponse.bind(this),
+      error: this.handleError.bind(this)
+    });
+  }
+
+  private checkpasswordStrengh(password: string) {
     if (!password || !this.firstName() || !this.lastName()) {
       this.passwordError = PasswordError.Empty;
       return false;
     }
 
     const hasUpperCase = /[A-Z]+/.test(password);
-
     const hasLowerCase = /[a-z]+/.test(password);
-
     const hasFirstName = password.includes(this.firstName());
-
     const hasLastName = password.includes(this.lastName());
-
-    console.log('Still here');
 
     if (!hasUpperCase || !hasLowerCase) {
       this.passwordError = PasswordError.Characters;
@@ -83,32 +90,22 @@ export class SignUpComponent {
     
     if (hasFirstName || hasLastName) {
       this.passwordError = PasswordError.NameUsage;
-      console.log('Still here');
       return false;
     }
 
     return true;
-}
+  }
 
-  onSubmit(): void {
-    this.isLoading = true;
-    const signUpDetails: SignUpDetails = {
-      firstName: this.firstName(),
-      lastName: this.lastName(),
-      email: this.email()
-    }
 
-    this.signUpService.postSignUpDetails(signUpDetails).subscribe((data: SignUpDetails) => {
-      setTimeout(() => {
-        console.log(data);
-        this.router.navigate(['confirmation']);
-        this.isLoading = false;
-        this
-      }, 2000);
-    }, err => {
-      this.isLoading = false;
-      console.log('this is the error: ', err)
-    });
+  private handleSignUpResponse(data: SignUpDetails) {
+    this.isLoading = false;
+    console.log('this is the data from the server: ', data)
+    this.router.navigate(['confirmation']);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    this.isLoading = false;
+    console.error('this is the error: ', error)
   }
 
 }
