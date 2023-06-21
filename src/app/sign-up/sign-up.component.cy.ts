930dfd57ac6999b171b14fd381f1d1c8e29721af
Toetenel, Rik (ITDV UD) - KLM <Rik.Toetenel@klm.com>
2023-06-21 23:15:31 +0200

    test(sign-up-form): Add test for emailadress validation

--- src/app/sign-up/sign-up.component.cy.ts
+++ src/app/sign-up/sign-up.component.cy.ts
@@ -24,7 +24,15 @@ describe(SignUpComponent.name, () => {
     cy.get('[data-cy="button"]').should('not.be.disabled');
   });
 
-  it('submit button should disabled when password has fewer than 8 chars', () => {
+  it('submit button should be disabled when email is not correct', () => {
+    cy.get('[data-cy="first-name"]').type('Rik');
+    cy.get('[data-cy="last-name"]').type('Toetenel');
+    cy.get('[data-cy="email"]').type('riktoetIsNoEmailAdress');
+    cy.get('[data-cy="password"]').type('Blablabla');
+    cy.get('[data-cy="button"]').should('be.disabled');
+  });
+
+  it('submit button should be disabled when password has fewer than 8 chars', () => {
     cy.get('[data-cy="first-name"]').type('Rik');
     cy.get('[data-cy="last-name"]').type('Toetenel');
     cy.get('[data-cy="email"]').type('riktoet@gmail.com');
@@ -40,7 +48,7 @@ describe(SignUpComponent.name, () => {
     cy.get('[data-cy="button"]').should('be.disabled');
   });
 
-  it('submit button should disabled when password has only uppercase chars', () => {
+  it('submit button should be disabled when password has only uppercase chars', () => {
     cy.get('[data-cy="first-name"]').type('Rik');
     cy.get('[data-cy="last-name"]').type('Toetenel');
     cy.get('[data-cy="email"]').type('riktoet@gmail.com');
@@ -48,7 +56,7 @@ describe(SignUpComponent.name, () => {
     cy.get('[data-cy="button"]').should('be.disabled');
   });
 
-  it('submit button should disabled when password has the first name in it', () => {
+  it('submit button should be disabled when password has the first name in it', () => {
     cy.get('[data-cy="first-name"]').type('Rik');
     cy.get('[data-cy="last-name"]').type('Toetenel');
     cy.get('[data-cy="email"]').type('riktoet@gmail.com');
@@ -56,7 +64,7 @@ describe(SignUpComponent.name, () => {
     cy.get('[data-cy="button"]').should('be.disabled');
   });
 
-  it('submit button should disabled when password has the first name in it', () => {
+  it('submit button should be disabled when password has the first name in it', () => {
     cy.get('[data-cy="first-name"]').type('Rik');
     cy.get('[data-cy="last-name"]').type('Toetenel');
     cy.get('[data-cy="email"]').type('riktoet@gmail.com');
@@ -64,7 +72,7 @@ describe(SignUpComponent.name, () => {
     cy.get('[data-cy="button"]').should('be.disabled');
   });
 
-  it('submit button should disabled when password has the first and last name in it', () => {
+  it('submit button should be disabled when password has the first and last name in it', () => {
     cy.get('[data-cy="first-name"]').type('Rik');
     cy.get('[data-cy="last-name"]').type('Toetenel');
     cy.get('[data-cy="email"]').type('riktoet@gmail.com');
@@ -72,7 +80,7 @@ describe(SignUpComponent.name, () => {
     cy.get('[data-cy="button"]').should('be.disabled');
   });
 
-  it('submit button should disabled first when password has the first and last name in it, but when you later edit the names it will be not be disabled', () => {
+  it('submit button should be disabled first when password has the first and last name in it, but when you later edit the names it will be not be disabled', () => {
     cy.get('[data-cy="first-name"]').type('Rik');
     cy.get('[data-cy="last-name"]').type('Toetenel');
     cy.get('[data-cy="email"]').type('riktoet@gmail.com');
